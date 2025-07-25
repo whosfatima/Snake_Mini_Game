5fee6f14fc8ba0d7c1dda8bc443ef89eedbc5994
maryam <fatimaisherewithyou@gmail.com>
2025-07-25 19:04:42 +0330

    Done ?

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const div = document.querySelector("div");
 const foodBox = document.querySelector(".food");
+let number= document.querySelector('h1')
 
-let step = 30;
+let step = 50;
 let x = 0;
 let y = 0;
 let direction = null;
 let directions = ["ArrowDown", "ArrowUp", "ArrowRight", "ArrowLeft"];
+let score = 0
 document.addEventListener("keydown", (event) => {
   if (directions.includes(event.key)) {
     direction = event.key;
@@ -26,6 +28,30 @@ let boxmovement = () => {
     x -= step;
     div.style.left = `${x}px`;
   }
+
+  if (x == foodBox.offsetLeft && y == foodBox.offsetTop) {
+    console.log("ate");
+    FoodPlace();
+    number.innerText= ++score
+  }
+};
+let foodmovement = () => {
+  let Xmove = window.innerWidth - step;
+  let Ymove = window.innerHeight - step;
+
+  let randomX = Math.floor(Math.random() * (Xmove / step)) * step;
+  let randomY = Math.floor(Math.random() * (Ymove / step)) * step;
+
+  return {
+    X: randomX,
+    Y: randomY,
+  };
 };
 
+let FoodPlace = () => {
+  let { X, Y } = foodmovement();
+  foodBox.style.left = `${X}px`;
+  foodBox.style.top = `${Y}px`;
+};
 setInterval(boxmovement, 200);
+window.addEventListener("load", FoodPlace);
